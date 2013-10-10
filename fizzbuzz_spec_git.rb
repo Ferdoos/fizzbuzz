a69dd0f917c53d1b355c61638e75cedf8711bc08
Jack Bryant <jackbryant597@gmail.com>
2013-10-10 14:36:18 +0100

    created new branch develop and added 3 new tests to spec file

--- fizzbuzz_spec_git.rb
+++ fizzbuzz_spec_git.rb
@@ -12,5 +12,18 @@ describe 'fizzbuzz' do
 	it 'tells me if a number is divisible by fifteen' do
 		expect(divisible_by_fifteen?(15)).to be_true
 	end
-	
+
+	context 'playing' do
+		it 'returns fizz if number is divisible by three' do
+			expect(fizzbuzz(3)).to eq 'fizz'
+		end
+
+		it 'returns buzz if number is divisible by five' do
+			expect(fizzbuzz(5)).to eq 'buzz'
+		end
+
+		it 'returns fizzbuzz if number if divisible by fifteen' do
+			expect(fizzbuzz(15)).to eq 'fizzbuzz'
+		end
+	end	
 end
