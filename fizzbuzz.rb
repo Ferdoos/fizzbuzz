f240985e020cb6477f73c9bdff6447ba3035a795
Will Bell <willbell@gmail.com>
2013-10-10 15:00:35 +0100

    added two methods, one incomplete

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,20 @@
 def divisible_by_three? number
-	number % 3 == 0
+	divisible_by? number, 3
 end
 
 def divisible_by_five? number
-	number % 5 == 0
+	divisible_by? number, 5
 end
 
 def divisible_by_fifteen? number
-	number % 15 == 0
+	divisible_by? number, 15
+end
+
+def divisible_by? number, divisor
+	number % divisor == 0
+end
+
+def fizzbuzz number
+	return "fizz" if divisible_by_three? number
+	return "buzz" if divisible_by_five? number
 end
