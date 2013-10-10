4b0d58289acd30e6234a3d677bd4aefb29977c62
Jack Bryant <jackbryant597@gmail.com>
2013-10-10 13:58:23 +0100

    added two more tests

--- fizzbuzz_spec_git.rb
+++ fizzbuzz_spec_git.rb
@@ -4,5 +4,13 @@ describe 'fizzbuzz' do
 	it 'tells me if a number is divisible by three' do
 		expect(divisible_by_three?(3)).to be_true
 	end
+
+	it 'tells if a number is divisible by five' do
+		expect(divisible_by_five?(5)).to be_true
+	end
+
+	it 'tells me if a number is divisible by fifteen' dp
+		expect(divisible_by_fifteen?(15)).to be_true
+	end
 	
 end
